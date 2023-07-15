c32e32bdfc7bfd45c246b79b3dacb4a27b7aee04
Zia Uddin Ahmed <zia.ete10@gmail.com>
2023-07-15 22:58:19 +0600

    Infra: Initilize azure cloud broker

---
+++ Bazaar.Core.Api.Infrastructure.Provision/Brokers/Clouds/CloudBroker.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using System;
+
+namespace Bazaar.Core.Api.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial class CloudBroker : ICloudBroker
+    {
+        private readonly string clientId;
+        private readonly string clientSecret;
+        private readonly string tenantId;
+        private readonly string adminName;
+        private readonly string adminAccess;
+        private readonly IAzure azure;
+
+        public CloudBroker()
+        {
+            this.clientId = Environment.GetEnvironmentVariable("AzureClientId");
+            this.clientSecret = Environment.GetEnvironmentVariable("AzureClientSecret");
+            this.tenantId = Environment.GetEnvironmentVariable("AzureTenantId");
+            this.adminName = Environment.GetEnvironmentVariable("AzureAdminName");
+            this.adminAccess = Environment.GetEnvironmentVariable("AzureAdminAccess");
+        }
+
+        private IAzure AuthenticateAzure()
+        {
+            AzureCredentials credentials = SdkContext.AzureCredentialsFactory.FromServicePrincipal(
+                 clientId: this.clientId,
+                 clientSecret: this.clientSecret,
+                 tenantId: this.tenantId,
+                 environment: AzureEnvironment.AzureChinaCloud);
+
+            return Azure.Configure()
+                  .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
+                  .Authenticate(credentials)
+                  .WithDefaultSubscription();
+
+        }
+    }
+}
---
+++ Bazaar.Core.Api.Infrastructure.Provision/Brokers/Clouds/ICloudBroker.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.Management.Fluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bazaar.Core.Api.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial class ICloudBroker
+    {
+    }
+}
