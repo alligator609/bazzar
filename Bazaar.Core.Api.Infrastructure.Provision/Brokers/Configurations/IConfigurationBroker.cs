e2cd944909b11d577970dd1016d20be4fee02dcb
Zia Uddin Ahmed <zia.ete10@gmail.com>
2023-07-18 23:32:14 +0600

    Broker: Implement Configuration

---
+++ Bazaar.Core.Api.Infrastructure.Provision/Brokers/Configurations/IConfigurationBroker.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bazaar.Core.Api.Infrastructure.Provision.Models.Configurations;
+
+namespace Bazaar.Core.Api.Infrastructure.Provision.Brokers.Configurations
+{
+    public interface IConfigurationBroker
+    {
+        CloudManagementConfiguration GetConfiguration();
+    }
+}
